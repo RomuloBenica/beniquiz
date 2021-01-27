a5e89378b2b67876ff2a4342c9675f56abeee1ab
RomuloBenica <romulobenica@hotmail.com>
2021-01-27 20:41:40 -0300

    aula 2 finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
 import styled from 'styled-components';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import QuizLogo from '../src/components/QuizLogo';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
+import Head from 'next/head';
 
 import db from '../db.json';
 
@@ -35,16 +38,38 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <QuizLogo></QuizLogo>
+      <Head>
+        <title>Beniquiz</title>
+
+      </Head>
       <QuizContainer>
+        <QuizLogo></QuizLogo>
         <Widget>
           <Widget.Header>
             <h1>Titulo</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Teste teste okok</p>
+            <form onSubmit={function Submit(event) {
+              event.preventDefault();
+
+              router.push(`/quiz?${name}`);
+            }}>
+              <input 
+                placeholder="Diga seu nome:" 
+                onChange={function (infosDeEvento) {
+                  setName(infosDeEvento.target.value)
+                }}
+                value={name}
+              />
+              <button type="submit" disabled={name.length === 0} >
+                Jogar, {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
